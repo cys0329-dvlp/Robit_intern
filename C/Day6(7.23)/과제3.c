1133a285698ddfe2105a58bd5e62fd28d60fd3c6
cys0329-dvlp <okkyuns0329@gmail.com>
2026-07-24 06:34:52 +0900

    Add files via upload

---
+++ C/Day6(7.23)/과제3.c
@@ -0,0 +1,198 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct _Node
+{
+    int data;
+    struct _Node* next;
+}Node;
+
+typedef struct _Queue
+{
+    Node* head;
+    Node* tail;
+    int size;
+}Queue;
+
+int Enqueue(Queue* pQu, int n);
+int Dequeue(Queue* pQu);
+int size(Queue* pQu);
+int front(Queue* pQu);
+int rear(Queue* pQu);
+int isEmpty(Queue* pQu);
+int printQueue(Queue* pQu);
+int string_compare(char a[], char b[]);
+
+int main()
+{
+    Queue qu = { NULL, NULL, 0 };
+    Queue* pQu = &qu;
+    while (1)
+    {
+        printf("==명령어를 입력하시오==\n");
+        int n = 1;
+        int d;
+        char oder[30];
+        scanf("%s", &oder);
+
+
+        if (string_compare(oder, "Enqueue") == 0)
+        {
+            scanf("%d", &d);
+            Enqueue(pQu, d);
+        }
+        else if (string_compare(oder, "Dequeue") == 0)
+        {
+            Dequeue(pQu);
+        }
+        else if (string_compare(oder, "size") == 0)
+        {
+            size(pQu);
+        }
+        else if (string_compare(oder, "front") == 0)
+        {
+            front(pQu);
+        }
+        else if (string_compare(oder, "rear") == 0)
+        {
+            rear(pQu);
+        }
+        else if (string_compare(oder, "isEmpty") == 0)
+        {
+            isEmpty(pQu);
+        }
+        else if (string_compare(oder, "printQueue") == 0)
+        {
+            printQueue(pQu);
+        }
+        else if (string_compare(oder, "exit") == 0)
+        {
+            break;
+        }
+        else {
+            printf("잘못된 명령어입니다.\n");
+        }
+    }
+    return 0;
+}
+int string_compare(char a[], char b[])
+{
+    int i = 0;
+    while (a[i] != '\0' || b[i] != '\0')
+    {
+        if (a[i] > b[i])
+        {
+            return -1;
+        }
+        else if (a[i] < b[i])
+        {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+int Enqueue(Queue* pQu, int n)
+{
+    Node* s = (Node*)malloc(sizeof(Node));
+    s->data = n;
+    s->next = NULL;   // 새 노드는 큐의 맨 뒤이므로 next는 항상 NULL
+
+    if (pQu->head == NULL)   // 큐가 비어있던 경우
+    {
+        pQu->head = s;
+        pQu->tail = s;
+    }
+    else                     // 큐에 이미 노드가 있는 경우
+    {
+        pQu->tail->next = s;  // 기존 tail의 next가 새 노드를 가리키도록 연결
+        pQu->tail = s;        // tail을 새 노드로 갱신
+    }
+    pQu->size++;
+    return 0;
+}
+
+int Dequeue(Queue* pQu)
+{
+    if (pQu->size == 0)
+    {
+        printf("Queue가 비어있습니다.\n");
+    }
+    else
+    {
+        Node* ptr = pQu->head;
+        pQu->head = pQu->head->next;
+        if (pQu->head == NULL)   // 마지막 노드를 삭제한 경우 tail도 NULL로 맞춰줌
+        {
+            pQu->tail = NULL;
+        }
+        printf("삭제된 값: %d\n", ptr->data);
+        free(ptr);
+        pQu->size--;
+    }
+    return 0;
+}
+
+int size(Queue* pQu)
+{
+    int a = pQu->size;
+    printf("Queue의 size: %d\n", a);
+    return a;
+}
+
+int front(Queue* pQu)
+{
+    if (pQu->size == 0)
+    {
+        printf("Queue가 비어있습니다.\n");
+        return -1;
+    }
+    printf("%d\n", pQu->head->data);
+    return pQu->head->data;
+}
+
+int rear(Queue* pQu)
+{
+    if (pQu->size == 0)
+    {
+        printf("Queue가 비어있습니다.\n");
+        return -1;
+    }
+    printf("%d\n", pQu->tail->data);
+    return pQu->tail->data;
+}
+
+int isEmpty(Queue* pQu)
+{
+    if (pQu->size == 0)
+    {
+        printf("True\n");
+        return 1;
+    }
+    else
+    {
+        printf("False\n");
+        return 0;
+    }
+}
+
+int printQueue(Queue* pQu)
+{
+    if (pQu->size == 0)
+    {
+        printf("Queue가 비어있습니다.\n");
+    }
+    else
+    {
+        printf("==Queue 안의 값==\n");
+        for (Node* p = pQu->head; p != NULL; p = p->next)   // head부터 tail 방향으로 출력 (들어온 순서대로)
+        {
+            printf("%d ", p->data);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ C/Day6(7.23)/과제4.c
@@ -0,0 +1,142 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Node
+{
+    char data;
+    struct _Node* next;
+} Node;
+
+typedef struct _Queue
+{
+    Node* head;
+    Node* tail;
+    int size;
+} Queue;
+
+typedef struct _Stack
+{
+    Node* top;
+    int size;
+} Stack;
+
+void Enqueue(Queue* pQu, char c);
+int Dequeue(Queue* pQu, char* out);
+void push(Stack* stk, char c);
+int pop(Stack* stk, char* out);
+
+int main()
+{
+    char c;
+    Queue qu = { NULL, NULL, 0 };
+    Queue* pQu = &qu;
+
+    Stack stack = { NULL, 0 };
+    Stack* stk = &stack;
+
+    printf("문자열 입력: ");
+
+    // 큐에 문자를 순서대로, 스택에도 동시에 넣음
+    while (1)
+    {
+        scanf("%c", &c);
+        if (c == '\n')   //엔터 치면 while문 종료 
+        {
+            break;
+        }
+        if (c == ' ')    // 공백은 카운트 X (양쪽에서 세면 어긋나기 때문에)
+        {
+            continue;
+        }
+        Enqueue(pQu, c);
+        push(stk, c);
+    }
+
+    int count = pQu->size;   // 큐 크기 = 공백을 제외한 문자 개수
+    int is_same = 1;         // 1이면 회문, 0이면 회문 아님
+
+    for (int i = 0; i < count; i++)
+    {
+        char front, top;
+        Dequeue(pQu, &front);   // 큐에서 앞에서부터 순서대로 꺼냄
+        pop(stk, &top);         // 스택에서 뒤에서부터(역순) 꺼냄
+
+        if (front != top) //문자가 하나라도 다르면 0출력하고 바로 for문 끝냄
+        {
+            is_same = 0;
+            break;
+        }
+    }
+
+    if (is_same == 1)
+    {
+        printf("회문입니다\n");
+    }
+    else
+    {
+        printf("회문이 아닙니다\n");
+    }
+
+    return 0;
+}
+
+void Enqueue(Queue* pQu, char c)
+{
+    Node* s = (Node*)malloc(sizeof(Node));
+    s->data = c;
+    s->next = NULL;
+
+    if (pQu->head == NULL)
+    {
+        pQu->head = s;
+        pQu->tail = s;
+    }
+    else
+    {
+        pQu->tail->next = s;
+        pQu->tail = s;
+    }
+    pQu->size++;
+}
+
+int Dequeue(Queue* pQu, char* out)
+{
+    if (pQu->head == NULL)
+    {
+        return 0;
+    }
+    Node* ptr = pQu->head;
+    *out = ptr->data;
+    pQu->head = pQu->head->next;
+    if (pQu->head == NULL)
+    {
+        pQu->tail = NULL;
+    }
+    free(ptr);
+    pQu->size--;
+    return 1;
+}
+
+void push(Stack* stk, char c)
+{
+    Node* s = (Node*)malloc(sizeof(Node));
+    s->data = c;
+    s->next = stk->top;   // 새 노드가 항상 맨 위(top)가 됨
+    stk->top = s;
+    stk->size++;
+}
+
+int pop(Stack* stk, char* out)
+{
+    if (stk->top == NULL)
+    {
+        return 0;
+    }
+    Node* ptr = stk->top;
+    *out = ptr->data;
+    stk->top = stk->top->next;
+    free(ptr);
+    stk->size--;
+    return 1;
+}
